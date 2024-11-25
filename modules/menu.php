053e1221e383e54dc6ee37754e3d5307b1212cbc
Truong Thien <truongthien144@gmail.com>
2024-11-25 21:13:56 +0700

    add cart, user on menu

--- modules/menu.php
+++ modules/menu.php
@@ -16,16 +16,26 @@
                 <img src="./img/download.png" alt="" width="112" height="112">
             </a>
             <form class="d-flex order-lg-1">
-                <input class="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search">
+                <input class="form-control" name="tim" id="input1" type="search" placeholder="Tìm kiếm" aria-label="Search">
                 <button class="btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <div class="dropdown">
+                    <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" onclick="dangxuat();">Đăng xuất</a></li>
+                        <li><a class="dropdown-item" href="change-pwd.php">Đổi mật khẩu</a></li>
+                    </ul>
+                </div>
+                <a class="fa--shopping-bag btn me-0" href="giohang.php" title="Xem giỏ hàng"><i class="fa-solid fa-cart-shopping"></i></a>
             </form>
-            <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse ms-3" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link active text-uppercase fs-6 fw-bold mx-4" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active text-uppercase fs-6 fw-bold mx-2" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link active dropdown-toggle text-uppercase fs-6 fw-bold mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link active dropdown-toggle text-uppercase fs-6 fw-bold mx-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             FOOTWEAR
                         </a>
                         <ul class="dropdown-menu bg-muted rounded-0" aria-labelledby="navbarDropdown">
@@ -35,7 +45,7 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link active dropdown-toggle text-uppercase fw-bold fs-6 mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link active dropdown-toggle text-uppercase fw-bold fs-6 mx-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             ACCESSORIES
                         </a>
                         <ul class="dropdown-menu bg-muted rounded-0" aria-labelledby="navbarDropdown">
@@ -45,7 +55,7 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link active dropdown-toggle text-uppercase fw-bold fs-6 mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link active dropdown-toggle text-uppercase fw-bold fs-6 mx-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Black friday sale
                         </a>
                         <ul class="dropdown-menu bg-muted rounded-0" aria-labelledby="navbarDropdown">
@@ -55,7 +65,7 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active text-uppercase fw-bold fs-6 mx-4" aria-current="page" href="https://www.google.com/">BLOG</a>
+                        <a class="nav-link active text-uppercase fw-bold fs-6 mx-2" aria-current="page" href="https://www.google.com/">BLOG</a>
                     </li>
                 </ul>
             </div>
